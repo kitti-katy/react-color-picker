bb585858a26470a1d9c6d73c4b34c3e50e5da98d
kitti-katy <kitti-katy@mail.ru>
2020-10-28 11:32:52 -0400

    converted to lib

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import styles from './styles.css'
-import ColorPicker_ from './ColorPicker_'
-
-export const ColorPicker = ({ baseColor, onChange, id, className }) => {
-  return <ColorPicker_ baseColor = {baseColor} onChange = {onChange} id={id} className={className}/>
-}
